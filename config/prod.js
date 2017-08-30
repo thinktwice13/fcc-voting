fee170e070863a91712592462dce7073cb993035
thinktwice13 <marioo.krajacic@gmail.com>
2017-08-30 17:06:18 +0200

    Setup passport-google authentication

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,5 @@
 module.exports = {
-  MONGO_URL: process.env.MONGO_URL
+  MONGO_URL: process.env.MONGO_URL,
+  googleClientId: process.env.GOOGLE_CLIENT_ID,
+  googleSecret: process.env.GOOGLE_SECRET
 }
--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const passport = require("passport")
+const GoogleStrategy = require("passport-google-oauth20")
+const keys = require("./config/keys")
 
-//include keys and secrets
-require("./config/keys")
+mongoose.connect(keys.MONGO_URL, { useMongoClient: true })
 
 //app config
 const app = express()
 const PORT = process.env.PORT || 3003
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientId,
+      clientSecret: keys.googleSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken)
+    }
+  )
+)
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+)
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google"),
+  (req, res) => {
+    res.redirect("/")
+  }
+)
+
+app.get("/", (req, res) => {
+  res.send("Hello")
+})
 
 app.listen(PORT, console.log("Server started."))
