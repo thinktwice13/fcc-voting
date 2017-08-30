78bf2bc90387becb3dedeb04200b458f4c9a7c99
thinktwice13 <marioo.krajacic@gmail.com>
2017-08-30 21:46:57 +0200

    Setup redux

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,15 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "materialize-css/dist/css/materialize.min.css"
 import App from "./components/App"
+import { Provider } from "react-redux"
+import { createStore, applyMiddleware } from "redux"
+import reducers from "./reducers"
 
-ReactDOM.render(<App />, document.querySelector("#root"))
+const store = createStore(reducers, {}, applyMiddleware())
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+)
