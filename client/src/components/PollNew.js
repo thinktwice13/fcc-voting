7ee3c040defd98aec911e75cecd62361783ca867
thinktwice13 <marioo.krajacic@gmail.com>
2017-08-31 12:35:16 +0200

    Create new poll form

--- client/src/components/PollNew.js
+++ client/src/components/PollNew.js
@@ -1,14 +1,60 @@
 import React from "react"
-import { reduxForm } from "redux-form"
+import { Link } from "react-router-dom"
+import { Field, FieldArray, reduxForm } from "redux-form"
 
-class PollNew extends React.Component {
-  render() {
-    return (
-      <div>Poll New Forms</div>
-    )
-  }
+const renderField = ({ input, label }) => (
+  <div>
+    <label>{label}</label>
+    <input {...input} />
+  </div>
+)
+
+const renderOptions = ({ fields, meta: { touched, error } }) => {
+  return (
+    <div>
+      <div>
+        {fields.map((option, i) => (
+          <Field
+            key={option}
+            name={option}
+            component={renderField}
+            label={"Option " + (i + 1)}
+          />
+        ))}
+      </div>
+      <div>
+        <button
+          className="btn-flat teal white-text"
+          type="button"
+          onClick={() => fields.push("")}
+          style={{ width: "100%", margin: "10px 0" }}
+        >
+          Add Option
+        </button>
+        {touched && error && <span>{error}</span>}
+      </div>
+    </div>
+  )
+}
+
+const PollNew = props => {
+  return (
+    <form>
+      <h4>Title</h4>
+      <Field
+        name="pollTitle"
+        type="text"
+        component={renderField}
+        label="Title"
+      />
+      <h4>Options</h4>
+      <FieldArray name="options" component={renderOptions} />
+      <a className="btn-flat red left white-text">Cancel</a>
+      <a className="btn-flat teal right white-text">Submit</a>
+    </form>
+  )
 }
 
 export default reduxForm({
-  form: "pollForm"
+  form: "pollNewForm"
 })(PollNew)
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from "redux"
 import user from "./user"
-import pollNew from "./pollNew"
+import { reducer as pollNew } from "redux-form"
 
 export default combineReducers({
   user,
-  pollNew
+  form: pollNew
 })
