b6875a1c6a9d5f01c363ac42c4c0818f0d61838a
thinktwice13 <marioo.krajacic@gmail.com>
2017-10-27 12:59:41 +0200

    Update HoCs

--- client/src/components/PollResults.js
+++ client/src/components/PollResults.js
@@ -1,10 +1,13 @@
 import React from "react"
 import Button from "react-md/lib/Buttons/Button"
+import { withAuth } from "../components/HoCs"
 
-export default ({ results }) => (
+const PollResults = ({ results }) => (
   <div className="flex-results">
     <Button flat disabled children={"MY VOTES: " + results.myVotes} />
     <Button flat disabled children={"MY POLLS: " + results.myPolls} />
     <Button flat disabled children={"MY POLLS VOTES: " + results.myPollVotes} />
   </div>
 )
+
+export default withAuth(PollResults)
